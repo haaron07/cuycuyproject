92d254233ce06af91a7bba5ddc0146bd1ffd2daa
Eddy2101 <eddygarcia210197@gmail.com>
2022-09-16 20:15:03 -0500

    base de datos subido a la nube

--- db.py
+++ db.py
@@ -6,10 +6,10 @@ import os
 load_dotenv()  # take environment variables from .env.
 
 # Mysql Settings
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'root'
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or ''
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'localhost' # localhost
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'flaskcrud'
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'gsgcorp_kevindev'
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or 'b;yhD@U4a&H6'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'gsgcorppe.com' # localhost
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'gsgcorp_prueba_ing'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # MySQL Connection
